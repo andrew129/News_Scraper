2e18cdc98478672d43d07e55a0fa58cfdf496794
Andrew Stiles <ndrwstls544@gmail.com>
2019-10-15 20:34:57 -0400

    added routes

--- public/app.js
+++ public/app.js
@@ -1,10 +1,27 @@
 $.getJSON('/articles', function(data) {
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         $("#articles").append("<p data-id='" + data[i]._id + "'>" + '<strong>' + data[i].title + '</strong>' + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
         let newButton = $('<button>').attr('data-id', data[i]._id).text('Add Comment').addClass('btn btn-primary w-100 comment')
         let newButtonTwo = $('<button>').attr('data-id', data[i]._id).text('Show Comments').addClass('btn btn-secondary w-100 show-comment')
         $('#articles').append(newButton, newButtonTwo)
     }
+})
+
+$('#scrape-article').on('click', function() {
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  })
+    .then(function(data){
+      console.log(data)
+      for (let i = 0; i < data.length; i++) {
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + '<strong>' + data[i].title + '</strong>' + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
+        let newButton = $('<button>').attr('data-id', data[i]._id).text('Add Comment').addClass('btn btn-primary w-100 comment')
+        let newButtonTwo = $('<button>').attr('data-id', data[i]._id).text('Show Comments').addClass('btn btn-secondary w-100 show-comment')
+        $('#articles').append(newButton, newButtonTwo)
+      }
+      location.reload();
+    });
 })
 
 $(document).on("click", ".comment", function() {
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 let app = express()
 
-mongoose.connect("process.env.MONGODB_URI" || "mongodb://localhost/webscraper", { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/webscraper", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(logger("dev"));
 
